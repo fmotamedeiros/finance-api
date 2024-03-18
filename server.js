2d8f48563b34208c2da9d546752b137367f5b49a
Flavio Medeiros <flavio@172-101-000-210.res.spectrum.com>
2024-03-18 11:12:00 -0300

    Adding support to cors

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const initializeDatabase = require('./src/config/dbInit');
 const userRoutes = require('./src/api/routes/userRoutes');
 const accountRoutes = require('./src/api/routes/accountRoutes');
@@ -9,6 +10,7 @@ const {serverPort} = require('./src/config/config');
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use('/api', userRoutes);
 app.use('/api', accountRoutes);
 app.use('/api', categoryRoutes);
